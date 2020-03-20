8638b29e43fb90dcb881214eee7913c096d87441
Ewa Mitra <ewa.mitra@gmail.com>
2020-03-20 09:09:21 +0100

    feat:homepageongulp

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,25 @@
-"use strict";
 
-// service worker registration - remove if you're not going to use it
+console.log('Siema');
+const name = 'Ewa';
+const age = 35;
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+
+console.log(`Nazywam się ${name} i mam ${age}lat. Cieszę się, że zaglądasz na moją stronę.`);
+
+const knowMore = document.querySelector('.knowMore__extra--js');
+knowMore.innerHTML = `Nazywam się Ewa Mitra.Teraz muszę to napisać, a potem skasuję`;
+
+const welcome = (name, age)=> {
+    console.log(`Witaj, ${name}! Masz ${age} lat`);
 }
+welcome (name,age);
 
-// place your code below
+const navigationSwitcher= document.querySelector('.navigation__switcher--js');
 
+navigationSwitcher.addEventListener('click', (e)=> {
+    const navigationList=document.querySelector('.navigation__list--js')
+    navigationList.classList.toggle('navigation__list--visible')  
+});
 
-console.log(`Hello world!`);
 
 
